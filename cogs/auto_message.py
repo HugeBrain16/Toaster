314ed10e50675709c5039779290b3ea28867bceb
HugeBrain16 <joshtuck373@gmail.com>
2021-04-02 13:58:08 -0700

    change smth

--- cogs/auto_message.py
+++ cogs/auto_message.py
@@ -4,33 +4,6 @@ import os, iniparser2, traceback, sys
 class AutoMessage(commands.Cog):
 	def __init__(self, client):
 		self.client = client
-
-	# https://gist.github.com/EvieePy/7822af90858ef65012ea500bcecf1612
-	@commands.Cog.listener()
-	async def on_command_error(self, ctx, error):
-		if hasattr(ctx.command, 'on_error'):
-		    return
-		cog = ctx.cog
-		if cog:
-		    if cog._get_overridden_method(cog.cog_command_error) is not None:
-		        return
-		ignored = (commands.CommandNotFound, )
-		error = getattr(error, 'original', error)
-		if isinstance(error, ignored):
-		    return
-		if isinstance(error, commands.DisabledCommand):
-		    await ctx.send(f'{ctx.command} has been disabled.')
-		elif isinstance(error, commands.NoPrivateMessage):
-		    try:
-		        await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
-		    except discord.HTTPException:
-		        pass
-		elif isinstance(error, commands.BadArgument):
-		    if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
-		        await ctx.send('I could not find that member. Please try again.')
-		else:
-		    print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-		    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 	
 	@commands.command()
 	async def addmsg(self, ctx, message, out):
@@ -38,12 +11,9 @@ class AutoMessage(commands.Cog):
 		if len(message) < 3: await ctx.send('message is too short! (min 3 char)'); return
 		_msg = iniparser2.INI('servers/message.ini').read()
 
-		if message not in _msg[str(ctx.guild.id)]:
-			_msg[str(ctx.guild.id)].update({message: out})
-		else:
-			_msg[str(ctx.guild.id)][message] = out
+		_msg[str(ctx.guild.id)].update({message: out}); iniparser2.INI('servers/message.ini').write(_msg)
 
-		iniparser2.INI('servers/message.ini').write(_msg); await ctx.send('new auto message has been set!')
+		await ctx.send('new auto message has been set!')
 
 	@commands.command()
 	async def delmsg(self,ctx,message):
